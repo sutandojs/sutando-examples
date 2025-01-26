197da1f58715a0c6e7b5ddc750436f885a92251e
Jeremy Holstein <jjjrmy@gmail.com>
2025-01-26 17:01:05 -0500

    update example

--- typescript/rest-hono-cf-d1/sutando.config.cjs
+++ typescript/rest-hono-cf-d1/sutando.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
     client: require('knex-cloudflare-d1'),
     connection: {
-      database: "test-sutando", // From Wrangler Binding, Defaults to first D1 Database in Wrangler.
-      wranglerPath: ".", // Default as "."
+      database: "test-sutando", // From Wrangler Binding
+      local: true, // Toggles `--local` flag on `wrangler d1 exec` command (Default as false)
     },
     useNullAsDefault: true,
     migrations: {
